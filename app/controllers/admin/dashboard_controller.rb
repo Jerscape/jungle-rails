42648f8a0d130821c4671c509293745b39cbcaca
@jerscape <jeremydutton5@gmail.com>
2024-01-26 18:36:47 -0500

    added http authenitication to admin/products page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+
+  #http authentication
+  http_basic_authenticate_with name: "Jungle", password: "book"
+  before_action :set_post, only: [:edit, :update, :destroy]
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,9 @@
 class Admin::ProductsController < ApplicationController
 
+  #http authentication
+  http_basic_authenticate_with name: "Jungle", password: "book"
+  before_action :set_post, only: [:edit, :update, :destroy]
+
   def index
     @products = Product.order(id: :desc).all
   end
