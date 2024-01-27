d4b12bb4771805bb0a5f95cdb17d750e9b56ef74
@jerscape <jeremydutton5@gmail.com>
2024-01-26 19:47:56 -0500

    total products and total categories displaying

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,22 @@
 class Admin::DashboardController < ApplicationController
 
+
   #http authentication
   http_basic_authenticate_with name: "Jungle", password: "book"
   before_action :set_post, only: [:edit, :update, :destroy]
 
+  # def fetch_stats
+  #   #fetch amount of products
+  #   @total_products = Product.count
+  #   #fetch total amount of caterories
+  #   @total_categories = Category.count
+    
+  # end
+
   def show
+      #fetch amount of products
+      @total_products = Product.count
+      #fetch total amount of caterories
+      @total_categories = Category.count
   end
 end
